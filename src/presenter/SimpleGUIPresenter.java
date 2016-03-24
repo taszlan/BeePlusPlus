30d853b9473c50813fe108f085e7545ae51eed05
Wojciech Kanas <wojciech.kanas@swmansion.com>
2016-03-24 19:34:48 +0100

    Podstawowe klasy dla prostego GUI

---
+++ src/presenter/SimpleGUIPresenter.java
@@ -0,0 +1,7 @@
+package presenter;
+
+/**
+ * Created by atticus on 24.03.16.
+ */
+public class SimpleGUIPresenter {
+}
---
+++ src/view/SimpleGIUMain.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by atticus on 24.03.16.
+ */
+public class SimpleGIUMain {
+}

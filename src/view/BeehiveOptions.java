d708580be10a923a7b515a5884595e0c13ee3402
malin <luk.malin@gmail.com>
2016-05-16 03:42:19 +0200

    zalazek menu

--- src/view/BeehiveOptions.java
+++ src/view/BeehiveOptions.java
@@ -10,5 +10,24 @@ import model.Beehive;
 import java.util.ArrayList;
 
 public class BeehiveOptions {
+	
+	private SimpleGUIPresenter simpleGUIPresenter;
+	private Beehive behiveForDetails;
 
+	public void setSimpleGUIPresenter(SimpleGUIPresenter simpleGUIPresenter) {
+		this.simpleGUIPresenter = simpleGUIPresenter;
+	}
+	
+	public SimpleGUIPresenter getSimpleGuiPresenter() {
+	        return simpleGUIPresenter;
+	    }
+	
+	public void display(int beehiveID){
+		
+		behiveForDetails = simpleGUIPresenter.getBeehiveWithID(beehiveID);
+		System.out.println("Ul printout");
+		System.out.println(behiveForDetails);
+	
+	}
+	
 }
